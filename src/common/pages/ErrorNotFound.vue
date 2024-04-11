a0d3a582b5074a4ba6e6984dfd4b7b07672d66b6
Roxnny A Barrios C <roxnny.barrios@gmail.com>
2024-04-11 19:14:53 -0400

    style: fix

--- src/common/pages/ErrorNotFound.vue
+++ src/common/pages/ErrorNotFound.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fullscreen bg-app-primary text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-app-tertiary text-white text-center q-pa-md flex flex-center">
     <div>
       <div style="font-size: 30vh">
         404
